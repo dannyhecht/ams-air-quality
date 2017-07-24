332a5a560a3d058275ec4389082ae1099a2e4be0
Danielle Hecht <dhecht@mit.edu>
2017-07-24 12:40:25 +0200

    Create import.py

---
+++ data_prep/import.py
@@ -0,0 +1,63 @@
+from collections import OrderedDict
+import csv
+import re
+import fileinput
+import sys
+
+def analyze_log(f):
+    stats = OrderedDict()
+    for line in f:
+        re.search(…)
+        …
+    return stats
+
+
+def write_stats(stats, f):
+    out = csv.writer(f)
+    out.writerow(…)
+    for var in stats:
+        out.writerow(…)
+
+def main(input_filename, output_filename):
+    with open(input_filename) as input_file:
+        stats = analyze_log(input_file)
+    with open(output_filename, 'w') as output_file:
+        write_stats(stats, output_file)
+        
+# PARSING        
+def analyze_log(f):
+    stats = OrderedDict()
+    for line in f:
+        _, _, rw_datatype, _, core_varname, _ = line.split()
+        match = re.search(r'.*[*\\](.*)', core_varname)
+        if not match:
+            continue
+        var = match.group(1)
+        match = re.search(r'([01])\\Global', core_varname)
+        core = match and match.group(1) or 'X'
+        rw, datatype = rw_datatype.split('-', 1)
+
+        var_stats = stats.get(var, {'rd': {'0': 0, '1': 0, 'X': 0},
+                                    'wr': {'0': 0, '1': 0, 'X': 0},
+                                    'type': datatype })
+        stats[var] = var_stats
+        var_stats[rw][core] += 1
+    return stats
+    
+    
+    # WRITING
+    def write_stats(stats, f):
+    out = csv.writer(f)
+    out.writerow(['Variable', 'Datatype',
+                  'CORE 0', None, 'CORE 1', None, 'CORE X', None])
+    out.writerow([None, None] +
+                 ['Read', 'Write'] * 3)
+    for var in stats:
+        out.writerow([var, stats[var]['type'],
+                      stats[var]['rd']['0'], stats[var]['wr']['0'],
+                      stats[var]['rd']['1'], stats[var]['wr']['1'],
+                      stats[var]['rd']['X'], stats[var]['wr']['X']])
+
+
+if __name__ == '__main__':
+    write_stats(analyze_log(fileinput.input()), sys.stdout)
