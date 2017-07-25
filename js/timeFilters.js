b9aa668aad9a0404bfee31ebf0cb2d0cc9f5fa0b
Danielle Hecht <dhecht@mit.edu>
2017-07-25 19:17:37 +0200

    Update timeFilters.js

--- js/timeFilters.js
+++ js/timeFilters.js
@@ -30,3 +30,17 @@ map.on('load', function() {
     }
   }, 'admin-2-boundaries-dispute'); // place the layer beneath this layer in the basemap
 });
+
+document.getElementById('slider').addEventListener('input', function(e) {
+  // get the current hour as an integer
+  var hour = parseInt(e.target.value);
+  // map.setFilter(layer-name, filter)
+  map.setFilter('collisions', ['==', 'Hour', hour]);
+
+  // converting 0-23 hour to AMPM format
+  var ampm = hour >= 12 ? 'PM' : 'AM';
+  var hour12 = hour % 12 ? hour % 12 : 12;
+  // update text in the UI
+  document.getElementById('active-hour').innerText = hour12 + ampm;
+});
+
