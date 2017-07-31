22ff444b1c153c4a9d51ee2ca703dd57ef12b8bf
Danielle Hecht <dhecht@mit.edu>
2017-07-31 12:53:15 +0200

    Update and rename geolocation.js to geolocate.js

---
+++ js/geolocate.js
@@ -0,0 +1,9 @@
+// Add geolocate control to the map.
+    map.addControl(new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    }));
+      
+    
