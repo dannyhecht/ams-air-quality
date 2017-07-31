52235a4da6dcc979623034dadbb5a109f4cb6597
Danielle Hecht <dhecht@mit.edu>
2017-07-31 16:45:12 +0200

    Update timeFilters.js

--- js/timeFilters.js
+++ js/timeFilters.js
@@ -79,7 +79,7 @@ map.on('load', function() {
     filter: ['==', 'Hour', 12],
     source: {
       type: 'geojson',
-      data: 'ams-air-quality/data/ams_stadhouderskade_5-01-2017.geojson' 
+      data: 'ams-air-quality/data/ams_stadhouderskade_5-01-2017_test.geojson' 
     },
     paint: {
       'circle-radius': {
