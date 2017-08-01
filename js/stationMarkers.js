2643af4423d868b35fb8cd5ea2fa37ef5afe5a64
Danielle Hecht <dhecht@mit.edu>
2017-08-01 12:45:32 +0200

    Update stationMarkers.js

--- js/stationMarkers.js
+++ js/stationMarkers.js
@@ -11,7 +11,146 @@ var stations1 = 'data/aqi_monitoring_network.geojson';
           map.addLayer({
             id: 'point',
             type: 'symbol',
-            source: stations1,
+            source: {  
+                                 {
+                   "type": "FeatureCollection",
+                   "features": [
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                       "type": "Point",
+                       "coordinates":  [ 4.904584,52.372234 ]
+                    },
+                    "properties": {
+                    "Station":"Amsterdam-Oude-Schans",
+                    "type":"City Background"
+                    }
+                  },
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                       "type": "Point",
+                       "coordinates":  [ 4.899696,52.358080 ]
+                    },
+                    "properties": {
+                    "Station":"Amsterdam-Stadhouderskade",
+                    "type":"Street Traffic"
+                    }
+                  },
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                       "type": "Point",
+                       "coordinates":  [ 4.866185,52.359864 ]
+                    },
+                    "properties": {
+                    "Station":"Amsterdam-Vondelpark",
+                    "type":"City Background"
+                    }
+                  },
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                       "type": "Point",
+                       "coordinates":  [ 4.860299,52.374851 ]
+                    },
+                    "properties": {
+                    "Station":"Amsterdam-Jan van Galenstraat",
+                    "type":"Street Traffic"
+                    }
+                  },
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                       "type": "Point",
+                       "coordinates":  [ 4.875582,52.385449 ]
+                    },
+                    "properties": {
+                    "Station":"Amsterdam-Haarlemmerweg",
+                    "type":"Street Traffic"
+                    }
+                  },
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                       "type": "Point",
+                       "coordinates":  [ 4.887904,52.390060 ]
+                    },
+                    "properties": {
+                    "Station":"Amsterdam-Van Diemenstraat",
+                    "type":"Street Traffic"
+                    }
+                  },
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                       "type": "Point",
+                       "coordinates":  [ 4.870355,52.394126 ]
+                    },
+                    "properties": {
+                    "Station":"Amsterdam-Westerpark",
+                    "type":"City Background"
+                    }
+                  },
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                       "type": "Point",
+                       "coordinates":  [ 4.845117,52.381797 ]
+                    },
+                    "properties": {
+                    "Station":"Amsterdam-Einsteinweg",
+                    "type":"Street Traffic"
+                    }
+                  },
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                       "type": "Point",
+                       "coordinates":  [ 4.841014,52.339669 ]
+                    },
+                    "properties": {
+                    "Station":"Amsterdam-A10-West",
+                    "type":"Street Traffic"
+                    }
+                  },
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                       "type": "Point",
+                       "coordinates":  [ 4.793309,52.367053 ]
+                    },
+                    "properties": {
+                    "Station":"Amsterdam-Ookmeer",
+                    "type":"City Background"
+                    }
+                  },
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                       "type": "Point",
+                       "coordinates":  [ 4.943648,52.389628 ]
+                    },
+                    "properties": {
+                    "Station":"Amsterdam-Nieuwendammerdijk",
+                    "type":"City Background"
+                    }
+                  },
+                  {
+                    "type": "Feature",
+                    "geometry": {
+                       "type": "Point",
+                       "coordinates":  [ 4.988346,52.320776 ]
+                    },
+                    "properties": {
+                    "Station":"Amsterdam -Kantershof",
+                    "type":"City Background"
+                    }
+                  }
+                ]
+                }   
+                  
+            },
             layout: {
               'icon-image': 'marker-15'
             },
