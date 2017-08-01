fa43443c31d0d632ab75dce03ffbaff6553a1d3e
Danielle Hecht <dhecht@mit.edu>
2017-08-01 14:03:13 +0200

    Update timeFilters.js

--- js/timeFilters.js
+++ js/timeFilters.js
@@ -1,75 +1,76 @@
-var url = '/data/ams_stadhouderskade_5-01-2017.geojson';
+var url = 'data/ams_vondelpark_5-01-2017.geojson';
 
-map.on('load', function() {
- var filterHour = ['==', 'Hour', 12]; 
- var filterDay = ['!=', 'Day', 'Bob'];
- var filterPollutant = ['==', 'Pollutant', 'NO2'];
- 
- map.getSource('pollutants').setData(url);
- map.addSource('pollutants', {
-            type: 'geojson',
-            data: url
-          });   
- map.addLayer({
-                id: 'pollutants',
-                type: 'circle',
-                filter: ['all', filterHour, filterDay, filterPollutant],
-                source: 'pollutants',
-                paint: {
-                  'circle-radius': {
-                    property: 'Concentration',
-                    stops: [
-                      [0, 3],
-                      [150, 15] ]    
-                  },
-                  'circle-color': {
-                    property: 'Concentration',
-                    stops: [
-                      [0, '#2DC4B2'],
-                      [25, '#3BB3C3'],
-                      [75, '#669EC4'],
-                      [100, '#8B88B6'],
-                      [125, '#A2719B'],
-                      [150, '#AA5E79'] ]
-                  },
-                  'circle-opacity': 0.8
+    map.on('load', function() {
+     var filterHour = ['==', 'Hour', 12]; 
+     var filterDay = ['!=', 'Day', 'Bob'];
+     var filterPollutant = ['==', 'Pollutant', 'NO2'];
 
-    }, 'admin-2-boundaries-dispute'}); // place the layer beneath this layer in the basemap
+     map.getSource('pollutants').setData(url);
+     map.addSource('pollutants', {
+                type: 'geojson',
+                data: url
+              });   
+     map.addLayer({
+                    id: 'pollutants',
+                    type: 'circle',
+                    filter: ['all', filterHour, filterDay, filterPollutant],
+                    source: 'pollutants',
+                    paint: {
+                      'circle-radius': {
+                        property: 'Concentration',
+                        stops: [
+                          [0, 3],
+                          [150, 15] ]    
+                      },
+                      'circle-color': {
+                        property: 'Concentration',
+                        stops: [
+                          [0, '#2DC4B2'],
+                          [25, '#3BB3C3'],
+                          [75, '#669EC4'],
+                          [100, '#8B88B6'],
+                          [125, '#A2719B'],
+                          [150, '#AA5E79'] ]
+                      },
+                      'circle-opacity': 0.8
 
- }); 
+        }, 'admin-2-boundaries-dispute'}); // place the layer beneath this layer in the basemap
 
-document.getElementById('slider').addEventListener('input', function(e) {
-                  // get the current hour as an integer 
+     }); 
 
-                  var hour = parseInt(e.target.value); //string argument
-                  //hour = hour < 10 ? '0' + '' + hour + ':00:00+02:00': hour;
-                  // map.setFilter(layer-name, filter)
-                  filterHour = ['==', 'Hour', hour];
-                  map.setFilter('pollutants', ['all', filterHour, filterDay, filterPollutant]); 
-                  
-                  
-                  
-                  // converting 0-23 hour to AMPM format
-                  var ampm = hour >= 12 ? 'PM' : 'AM';
-                  var hour12 = hour % 12 ? hour % 12 : 12;
-                  // update text in the UI
-                  document.getElementById('active-hour').innerText = hour12 + ampm;
-});
+    document.getElementById('slider').addEventListener('input', function(e) {
+                      // get the current hour as an integer 
 
-document.getElementById('filters').addEventListener('change', function(e) {
-                  var day = e.target.value;
-                  if (day === 'all') {
-                    // `null` would not work for combining filters
-                    filterDay = ['!=', 'Day', 'Bob'];
-                  } else if (day === 'weekday') {
-                    filterDay = ['!in', 'Day', 'Sat', 'Sun'];
-                  } else if (day === 'weekend') {
-                    filterDay = ['in', 'Day', 'Sat', 'Sun'];
-                  } else {
-                    console.log('error');
-                  }
-                  map.setFilter('pollutants', filterHour, filterDay, filterPollutant);
-});
+                      var hour = parseInt(e.target.value); //string argument
+                      //hour = hour < 10 ? '0' + '' + hour + ':00:00+02:00': hour;
+                      // map.setFilter(layer-name, filter)
+                      filterHour = ['==', 'Hour', hour];
+                      map.setFilter('pollutants', ['all', filterHour, filterDay, filterPollutant]); 
+
+
+
+                      // converting 0-23 hour to AMPM format
+                      var ampm = hour >= 12 ? 'PM' : 'AM';
+                      var hour12 = hour % 12 ? hour % 12 : 12;
+                      // update text in the UI
+                      document.getElementById('active-hour').innerText = hour12 + ampm;
+    });
+
+    document.getElementById('filters').addEventListener('change', function(e) {
+                      var day = e.target.value;
+                      if (day === 'all') {
+                        // `null` would not work for combining filters
+                        filterDay = ['!=', 'Day', 'Bob'];
+                      } else if (day === 'weekday') {
+                        filterDay = ['!in', 'Day', 'Sat', 'Sun'];
+                      } else if (day === 'weekend') {
+                        filterDay = ['in', 'Day', 'Sat', 'Sun'];
+                      } else {
+                        console.log('error');
+                      }
+                      map.setFilter('pollutants', filterHour, filterDay, filterPollutant);
+    });   
+
 
 
 /*
