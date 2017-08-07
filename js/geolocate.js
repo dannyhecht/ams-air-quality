8bac5a85997a077c18db4a23fe081ed22310644c
Danielle Hecht <dhecht@mit.edu>
2017-08-07 11:45:13 +0200

    Update geolocate.js

--- js/geolocate.js
+++ js/geolocate.js
@@ -1,3 +1,4 @@
+/* MAPBOXGL
 // Add geolocate control to the map.
     map.addControl(new mapboxgl.GeolocateControl({
         positionOptions: {
@@ -5,5 +6,32 @@
         },
         trackUserLocation: true
     }));
+*/
+
+// LEAFLET
+var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='Map data © <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+var osm = new L.TileLayer(osmUrl, {
+    attribution: osmAttrib,
+    detectRetina: true
+});
+
+// please replace this with your own mapbox token!
+var token = 'pk.eyJ1IjoiZGhlY2h0IiwiYSI6ImNqNHRueTVyeDA3ZmYyd3FuY2NmYW9tNmoifQ.FetU2-IBDcrhTmSKBpFIfA';
+var mapboxUrl = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}@2x?access_token=' + token;
+var mapboxAttrib = 'Map data © <a href="http://osm.org/copyright">OpenStreetMap</a> contributors. Tiles from <a href="https://www.mapbox.com">Mapbox</a>.';
+var mapbox = new L.TileLayer(mapboxUrl, {
+  attribution: mapboxAttrib,
+  tileSize: 512,
+  zoomOffset: -1
+});
+
+// add location control to global name space for testing only
+// on a production site, omit the "lc = "!
+lc = L.control.locate({
+    strings: {
+        title: "Show me where I am, yo!"
+    }
+}).addTo(map);
       
     
