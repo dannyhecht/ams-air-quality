9156d1751b55ad331fd27df5448429904dd3c636
Danielle Hecht <dhecht@mit.edu>
2017-08-07 11:52:10 +0200

    Update geolocate.js

--- js/geolocate.js
+++ js/geolocate.js
@@ -8,31 +8,7 @@
     }));
 */
 
-// LEAFLET
-var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib='Map data © <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
-var osm = new L.TileLayer(osmUrl, {
-    attribution: osmAttrib,
-    detectRetina: true
-});
-
-// please replace this with your own mapbox token!
-var token = 'pk.eyJ1IjoiZGhlY2h0IiwiYSI6ImNqNHRueTVyeDA3ZmYyd3FuY2NmYW9tNmoifQ.FetU2-IBDcrhTmSKBpFIfA';
-var mapboxUrl = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}@2x?access_token=' + token;
-var mapboxAttrib = 'Map data © <a href="http://osm.org/copyright">OpenStreetMap</a> contributors. Tiles from <a href="https://www.mapbox.com">Mapbox</a>.';
-var mapbox = new L.TileLayer(mapboxUrl, {
-  attribution: mapboxAttrib,
-  tileSize: 512,
-  zoomOffset: -1
-});
-
-var map = new L.Map('map', {
-    layers: [mapbox],
-    center: [52.35963, 4.885431],
-    zoom: 11.5,
-    zoomControl: true
-});
-
+// LEAFLET + MAPBOX
 // add location control to global name space for testing only
 // on a production site, omit the "lc = "!
 lc = L.control.locate({
