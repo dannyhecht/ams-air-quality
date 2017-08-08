fdeca6b135fe07c10cd879474d5e9fc29179a0ef
Danielle Hecht <dhecht@mit.edu>
2017-08-08 14:01:41 +0200

    Update aqi_monitoring_network.js

--- data/aqi_monitoring_network.js
+++ data/aqi_monitoring_network.js
@@ -9,6 +9,9 @@ var stations = {
        "coordinates":  [ 4.904584,52.372234 ]
     },
     "properties": {
+    "marker-color": "#7e7e7e",
+    "marker-size": "medium",
+    "marker-symbol": "",
     "Station":"Amsterdam-Oude-Schans",
     "des":"City Background"
     }
@@ -20,6 +23,9 @@ var stations = {
        "coordinates":  [ 4.899696,52.358080 ]
     },
     "properties": {
+    "marker-color": "#7e7e7e",
+    "marker-size": "medium",
+    "marker-symbol": "",   
     "Station":"Amsterdam-Stadhouderskade",
     "des":"Street Traffic"
     }
@@ -31,6 +37,9 @@ var stations = {
        "coordinates":  [ 4.866185,52.359864 ]
     },
     "properties": {
+    "marker-color": "#7e7e7e",
+    "marker-size": "medium",
+    "marker-symbol": "",   
     "Station":"Amsterdam-Vondelpark",
     "des":"City Background"
     }
@@ -42,6 +51,9 @@ var stations = {
        "coordinates":  [ 4.860299,52.374851 ]
     },
     "properties": {
+    "marker-color": "#7e7e7e",
+    "marker-size": "medium",
+    "marker-symbol": "",   
     "Station":"Amsterdam-Jan van Galenstraat",
     "des":"Street Traffic"
     }
@@ -53,6 +65,9 @@ var stations = {
        "coordinates":  [ 4.875582,52.385449 ]
     },
     "properties": {
+    "marker-color": "#7e7e7e",
+    "marker-size": "medium",
+    "marker-symbol": "",
     "Station":"Amsterdam-Haarlemmerweg",
     "des":"Street Traffic"
     }
@@ -64,6 +79,9 @@ var stations = {
        "coordinates":  [ 4.887904,52.390060 ]
     },
     "properties": {
+    "marker-color": "#7e7e7e",
+    "marker-size": "medium",
+    "marker-symbol": "",
     "Station":"Amsterdam-Van Diemenstraat",
     "des":"Street Traffic"
     }
@@ -75,6 +93,9 @@ var stations = {
        "coordinates":  [ 4.870355,52.394126 ]
     },
     "properties": {
+    "marker-color": "#7e7e7e",
+    "marker-size": "medium",
+    "marker-symbol": "",
     "Station":"Amsterdam-Westerpark",
     "des":"City Background"
     }
@@ -86,6 +107,9 @@ var stations = {
        "coordinates":  [ 4.845117,52.381797 ]
     },
     "properties": {
+    "marker-color": "#7e7e7e",
+    "marker-size": "medium",
+    "marker-symbol": "",
     "Station":"Amsterdam-Einsteinweg",
     "des":"Street Traffic"
     }
@@ -97,6 +121,9 @@ var stations = {
        "coordinates":  [ 4.841014,52.339669 ]
     },
     "properties": {
+    "marker-color": "#7e7e7e",
+    "marker-size": "medium",
+    "marker-symbol": "",
     "Station":"Amsterdam-A10-West",
     "des":"Street Traffic"
     }
@@ -108,6 +135,9 @@ var stations = {
        "coordinates":  [ 4.793309,52.367053 ]
     },
     "properties": {
+    "marker-color": "#7e7e7e",
+    "marker-size": "medium",
+    "marker-symbol": "",
     "Station":"Amsterdam-Ookmeer",
     "des":"City Background"
     }
@@ -119,6 +149,9 @@ var stations = {
        "coordinates":  [ 4.943648,52.389628 ]
     },
     "properties": {
+    "marker-color": "#7e7e7e",
+    "marker-size": "medium",
+    "marker-symbol": "",
     "Station":"Amsterdam-Nieuwendammerdijk",
     "des":"City Background"
     }
@@ -130,6 +163,9 @@ var stations = {
        "coordinates":  [ 4.988346,52.320776 ]
     },
     "properties": {
+    "marker-color": "#7e7e7e",
+    "marker-size": "medium",
+    "marker-symbol": "",
     "Station":"Amsterdam -Kantershof",
     "des":"City Background"
     }
